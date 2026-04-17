109fb96f2e6b3f8172d12c536de0ab4e0631443c
asty777 <nikadek010933@gmail.com>
2026-04-17 13:26:31 +0700

    update admin_features

--- admin/games/admin_features.py
+++ admin/games/admin_features.py
@@ -1,8 +1,42 @@
 from database.utils import read_file, write_file
 
-# =========================
-# LOAD & SAVE GAMES
-# =========================
+# tampilkan game
+def lihat():
+    games = load_games()
+
+    if not games:
+        print("Belum ada game.")
+        return
+
+    print("\n===== DATA GAME =====")
+    for g in games:
+        print(g)
+
+
+#tambah game
+def tambah():
+    games = load_games()
+
+    nama = input("Nama: ")
+    genre = input("Genre: ")
+
+    new_id = len(games) + 1
+
+    games.append({
+        "id": new_id,
+        "nama_game": nama,
+        "genre": genre,
+        "rating": 0,
+        "total_rating": 0,
+        "jumlah_rating": 0,
+        "played": 0,
+        "downloads": 0
+    })
+
+    save_games(games)
+    print("✅ Game berhasil ditambahkan!")
+
+
 def load_games():
     games = []
     data = read_file("games.txt")
@@ -40,55 +74,6 @@ def save_games(games):
         data.append(line)
 
     write_file("games.txt", data)
-
-
-#menu 
-def admin_menu():
-    while True:
-        print("\n1. Tambah Game")
-        print("2. Update Game")
-        print("3. Delete Game")
-        print("4. Lihat Game")
-        print("5. Exit")
-
-        pilih = input("Pilih: ")
-
-        if pilih == "1":
-            tambah()
-        elif pilih == "2":
-            update()
-        elif pilih == "3":
-            delete()
-        elif pilih == "4":
-            lihat()
-        elif pilih == "5":
-            break
-        else:
-            print("Pilihan tidak valid!")
-
-
-#tambah game
-def tambah():
-    games = load_games()
-
-    nama = input("Nama: ")
-    genre = input("Genre: ")
-
-    new_id = len(games) + 1
-
-    games.append({
-        "id": new_id,
-        "nama_game": nama,
-        "genre": genre,
-        "rating": 0,
-        "total_rating": 0,
-        "jumlah_rating": 0,
-        "played": 0,
-        "downloads": 0
-    })
-
-    save_games(games)
-    print("✅ Game berhasil ditambahkan!")
 
 
 # update game
@@ -135,14 +120,27 @@ def delete():
     print("✅ Game berhasil dihapus!")
 
 
-# tampilkan game
-def lihat():
-    games = load_games()
 
-    if not games:
-        print("Belum ada game.")
-        return
+#menu 
+def admin_menu():
+    while True:
+        print("\n1. Tambah Game")
+        print("2. Update Game")
+        print("3. Delete Game")
+        print("4. Lihat Game")
+        print("5. Exit")
 
-    print("\n===== DATA GAME =====")
-    for g in games:
-        print(g)
+        pilih = input("Pilih: ")
+
+        if pilih == "1":
+            tambah()
+        elif pilih == "2":
+            update()
+        elif pilih == "3":
+            delete()
+        elif pilih == "4":
+            lihat()
+        elif pilih == "5":
+            break
+        else:
+            print("Pilihan tidak valid!")
